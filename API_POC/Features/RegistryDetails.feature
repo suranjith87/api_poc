67e6a0ca776cd01cd2825fedbc51c132f95a3661
Suranjith Rodrigo <suranjith@ascentic.se>
2022-08-30 05:52:17 +0530

    Adds test format with specflow intergration

---
+++ API_POC/Features/RegistryDetails.feature
@@ -0,0 +1,26 @@
+﻿@API
+Feature: API_Carbon_Registry
+		As a user, 
+		I want to verify that these carbon credits are certified and registered with a carbon credits registry
+
+@Regression
+Scenario: API-TC01-Verify_that_the_catagory_name
+	Given I have access the api end point of registry
+	When I access the details of catagory
+	Then I should be able to catagory name as "Carbon credits"
+
+@Regression
+Scenario: API-TC02-Verify_that_the_catagory_can_re-list
+	Given I have access the api end point of registry
+	When I access the details of catagory
+	Then I should be able to view catagory re-list status as "true" 
+
+@Regression
+Scenario Outline: API-TC03-Verify_that_the_catagory_name
+	Given I have access the api end point of registry
+	When I access the details of catagory
+	Then I should be able to view promotion name as "<Name>" has a Description that contains the text "<Description>"
+
+	Examples:
+	| Name    | Description               |
+	| Gallery | Good position in category |
--- API_POC/Tests/RegistryTest.cs
+++ API_POC/Tests/RegistryTest.cs
@@ -19,19 +19,64 @@ namespace API_POC.Tests
         private RestResponse _response;
         private Registry? _dTORegistry;
 
-        [Test]
-        public void VerifytheNameoftheCategory()
+        [SetUp]
+        public void Setup()
         {
             _client = Helper.SetClient();
             // arrange
-            _request = Helper.CreateGetRequest("/v1/Categories/6327/Details.json?catalogue=false");
+            var request = Helper.CreateGetRequest("/v1/Categories/6327/Details.json?catalogue=false");
             // act
-            _response = Helper.CreateGetResponse(_client, _request);
+            _response = Helper.CreateGetResponse(_client, request);
             _dTORegistry = JsonConvert.DeserializeObject<Registry>(_response.Content);
-            // Assert
-            Assert.That(_response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-            Assert.That(_dTORegistry.Name, Is.EqualTo("Carbon credits"));
-            Assert.That(_dTORegistry.CanRelist, Is.EqualTo(true));
+        }
+
+        [TestCase("Carbon credits", HttpStatusCode.OK, TestName = "Verify the name of the category")]
+        [Test]
+        public void VerifytheNameoftheCategory(string name, HttpStatusCode expectedHttpStatusCode)
+        {   
+            // assert
+            Assert.That(_response.StatusCode, Is.EqualTo(expectedHttpStatusCode));
+            Assert.That(_dTORegistry.Name, Is.EqualTo(name));
+        }
+
+        [TestCase(true, HttpStatusCode.OK, TestName = "Verify category can re-list")]
+        public void VerifyCategoryCanRelist(bool status, HttpStatusCode expectedHttpStatusCode)
+        {
+            // assert
+            Assert.That(_response.StatusCode, Is.EqualTo(expectedHttpStatusCode));
+            Assert.That(_dTORegistry.CanRelist, Is.EqualTo(status));
+        }
+
+        [TestCase("Gallery", "Good position in category", HttpStatusCode.OK, TestName = "Verify the promotion name with description")]
+        public void VerifyRegistryInfo(string name, string description, HttpStatusCode expectedHttpStatusCode)
+        {
+            // assert
+            Assert.That(_response.StatusCode, Is.EqualTo(expectedHttpStatusCode));
+
+            Assert.Multiple(() =>
+            {
+                var status = false;
+                for (int i = 0; i < _dTORegistry.Promotions.Count; i++)
+                {
+                    if (name.Equals(_dTORegistry.Promotions[i].Name))
+                    {
+                        Assert.That(_dTORegistry.Promotions[i].Name, Is.EqualTo(name));
+                        Assert.That(_dTORegistry.Promotions[i].Description, Is.EqualTo(description), "Description does not match with the given content");
+                        status = true;
+                        break;
+                    }
+                }
+                if (!status)
+                {
+                    Assert.Fail("Category not listed in the registry");
+                }
+            });
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            Console.WriteLine("Verification points completed!");
         }
     }
 }
